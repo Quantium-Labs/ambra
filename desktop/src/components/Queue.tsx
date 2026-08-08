08161d5339333cd92cddd9d59339a2705a855cc5
eggwithpants <alex.n.rivkin@gmail.com>
2026-08-08 19:56:30 -0400

    more queue improvements, hopefully ready for actual shuffle, playback from queue, reordering, etc.

--- desktop/src/components/Queue.tsx
+++ desktop/src/components/Queue.tsx
@@ -4,21 +4,39 @@ import type { QueueEntry } from "../types/music";
 type QueueProps = {
   currentEntry: QueueEntry | undefined;
   upcomingEntries: QueueEntry[];
+  // track: Track[];
 };
 
 export function Queue({ currentEntry, upcomingEntries }: QueueProps) {
   return (
     <div id="queueList">
       <div id="currentQueueTrack">
+        <span id="currentTitle">Currently Playing</span>
         {currentEntry && (
-          <div className="queueTrack">{currentEntry.track.name}</div>
+          <div className="queueItem">
+            <div className="coverImgContainer">
+              <img
+                src={currentEntry.track.cover}
+                alt={`${currentEntry.track.album} album cover`}
+                className="coverImg"
+              />
+            </div>
+            <span className="trackName">{currentEntry.track.name}</span>
+          </div>
         )}
       </div>
-
       <div id="upcomingQueueTracks">
+        <span id="upcomingTitle">Upcoming</span>
         {upcomingEntries.map((entry) => (
-          <div key={`queue:${entry.queueId}`} className="queueTrack">
-            {entry.track.name}
+          <div className="queueItem">
+            <div className="coverImgContainer">
+              <img
+                src={entry.track.cover}
+                alt={`${entry.track.album} album cover`}
+                className="coverImg"
+              />
+            </div>
+            <span className="trackName">{entry.track.name}</span>
           </div>
         ))}
       </div>
